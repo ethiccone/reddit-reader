e37dbc4c613c7435dc96369f604215d18b3d2188
Igor B <igor.developers.rii@gmail.com>
2016-06-09 14:55:57 +0300

    add config data for collections

--- config/configs/db.js
+++ config/configs/db.js
@@ -2,6 +2,13 @@
 
 module.exports = {
 	mongo: {
-		connectionString: process.env.MONGO_CONN_STR || 'mongodb://localhost/redditReader'
+		connectionString: process.env.MONGO_CONN_STR || 'mongodb://localhost/redditReader',
+		collectionsSettings: {
+			articles: {
+				createdAt: {
+					expireAfterSeconds: 3600
+				}
+			}
+		}
 	}
 };
