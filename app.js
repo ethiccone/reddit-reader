a7ecfbb61dab98e1102905b5bff30a4ea63ea66f
Igor B <igor.developers.rii@gmail.com>
2016-06-09 14:55:20 +0300

    add http-error to app

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var HTTPError = require('http-errors');
+
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -23,13 +24,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(HTTPError.NotFound('Not Found'));
 });
 
 // error handlers
